398fb59ae5824e424517ee77454d3851505150c1
hsadiq <ksadik320@gmail.com>
2023-04-29 08:41:28 +0530

    Added Find Freuqency Code Using HashMap

---
+++ src/com/bl/Hashmap/HashMaps.java
@@ -0,0 +1,22 @@
+package com.bl.Hashmap;
+
+import java.util.HashMap;
+
+public class HashMaps {
+    public static void main(String[] args) {
+        HashMap<String, Integer> map = new HashMap<>();
+
+        String sentene = "To be or to be not";
+        String[] words =sentene.toLowerCase().split(" ");
+
+        for(String word: words){
+            Integer Value = map.get(word);
+            if(Value == null){
+                Value = 0;
+            }
+            Value +=1;
+            map.put(word,Value);
+        }
+
+    }
+}
